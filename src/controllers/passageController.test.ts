a3876cf61b7204d4e9184db0fd25b1fce4de9125
Jorge-1201458 <1201458@isep.ipp.pt>
2023-10-27 19:12:13 +0100

    Passage Tests

--- src/controllers/passageController.test.ts
+++ src/controllers/passageController.test.ts
@@ -5,10 +5,11 @@ import config from '../../config';
 import { Result } from '../core/logic/Result';
 import IPassageService from '../services/IServices/IPassageService';
 import PassageController from './passageController';
-import {IPassageDTO} from '../dto/IPassageDTO';
+import { IPassageDTO } from '../dto/IPassageDTO';
 import PassageRepo from '../repos/passageRepo';
 import BuildingRepo from '../repos/buildingRepo';
 import FloorRepo from '../repos/floorRepo';
+import url from 'url';
 describe('passage contoller', function () {
     beforeEach(function () { });
     it('createPassage: returns json with building1+building2+pisobuilding1+pisobuilding2', async function () {
@@ -25,94 +26,111 @@ describe('passage contoller', function () {
         };
         let next: Partial<NextFunction> = () => { };
 
-        let passageRepoInstance= Container.get(PassageRepo);
+        let passageRepoInstance = Container.get(PassageRepo);
         Container.set('PassageRepo', passageRepoInstance);
-        let buildingRepoInstance= Container.get(BuildingRepo);
+        let buildingRepoInstance = Container.get(BuildingRepo);
         Container.set('BuildingRepo', buildingRepoInstance);
-        let floorRepoInstance= Container.get(FloorRepo);
+        let floorRepoInstance = Container.get(FloorRepo);
         Container.set('FloorRepo', floorRepoInstance);
         let passageServiceClass = require(config.services.passage.path).default;
         let passageServiceInstance = Container.get(passageServiceClass);
         Container.set(config.services.passage.name, passageServiceInstance);
 
         passageServiceInstance = Container.get(config.services.passage.name);
-        sinon.stub(passageServiceInstance, 'createPassage').returns(Result.ok<IPassageDTO>({ id:'sad', building1: 'A', building2: 'B', pisobuilding1: 'A1', pisobuilding2: 'B2', positionBuilding1:[],positionBuilding2:[]}));
+        sinon.stub(passageServiceInstance, 'createPassage').returns(Result.ok<IPassageDTO>({ id: 'sad', building1: 'A', building2: 'B', pisobuilding1: 'A1', pisobuilding2: 'B2', positionBuilding1: [], positionBuilding2: [] }));
         const ctrl = new PassageController(passageServiceInstance as IPassageService);
         await ctrl.createPassage(<Request>req, <Response>res, <NextFunction>next);
         sinon.assert.calledOnce(res.json);
         sinon.assert.calledWith(res.json, sinon.match({
-            id:'sad',
+            id: 'sad',
             building1: 'A',
             building2: 'B',
             pisobuilding1: 'A1',
             pisobuilding2: 'B2',
-            positionBuilding1:[],
-            positionBuilding2:[]
+            positionBuilding1: [],
+            positionBuilding2: []
         }));
     });
     it('updatePassage: returns json with id+building1+building2+pisobuilding1+pisobuilding2', async function () {
-        let body = {   
-            id:'sad',
+        let body = {
+            id: 'sad',
             building1: 'A',
             building2: 'B',
             pisobuilding1: 'A1',
             pisobuilding2: 'B2'
-         };
+        };
         let req: Partial<Request> = {};
         req.body = body;
         let res: Partial<Response> = {
             json: sinon.spy(),
         };
         let next: Partial<NextFunction> = () => { };
-        let passageRepoInstance= Container.get(PassageRepo);
+        let passageRepoInstance = Container.get(PassageRepo);
         Container.set('PassageRepo', passageRepoInstance);
         let passageServiceClass = require(config.services.passage.path).default;
         let passageServiceInstance = Container.get(passageServiceClass);
         Container.set(config.services.passage.name, passageServiceInstance);
         passageServiceInstance = Container.get(config.services.passage.name);
-        sinon.stub(passageServiceInstance, 'updatePassage').returns(Result.ok<IPassageDTO>({ id:'sad', building1: 'A', building2: 'B', pisobuilding1: 'A1', pisobuilding2: 'B2', positionBuilding1:[],positionBuilding2:[]}));
+        sinon.stub(passageServiceInstance, 'updatePassage').returns(Result.ok<IPassageDTO>({ id: 'sad', building1: 'A', building2: 'B', pisobuilding1: 'A1', pisobuilding2: 'B2', positionBuilding1: [], positionBuilding2: [] }));
         const ctrl = new PassageController(passageServiceInstance as IPassageService);
         await ctrl.updatePassage(<Request>req, <Response>res, <NextFunction>next);
         sinon.assert.calledOnce(res.json);
         sinon.assert.calledWith(res.json, sinon.match({
-            id:'sad',
+            id: 'sad',
             building1: 'A',
             building2: 'B',
             pisobuilding1: 'A1',
             pisobuilding2: 'B2',
-            positionBuilding1:[],
-            positionBuilding2:[]
+            positionBuilding1: [],
+            positionBuilding2: []
         }));
     });
     it('getPassagesBetween2Buildings: returns json with building1+building2', async function () {
-        let body = {   
+        let body = {
             building1: 'A',
             building2: 'B',
-         };
-        let req: Partial<Request> = {};
-        req.body = body;
+        };
+        let req: Partial<Request> = {
+            url: url.format({
+                pathname: '/:building1/:building2',
+                query: body
+            })
+        };
         let res: Partial<Response> = {
             json: sinon.spy(),
         };
         let next: Partial<NextFunction> = () => { };
-        let passageRepoInstance= Container.get(PassageRepo);
-        Container.set('PassageRepo', passageRepoInstance);
-        let passageServiceClass = require(config.services.passage.path).default;
+        let passageServiceClass= require(config.services.passage.path).default;
         let passageServiceInstance = Container.get(passageServiceClass);
         Container.set(config.services.passage.name, passageServiceInstance);
         passageServiceInstance = Container.get(config.services.passage.name);
-        sinon.stub(passageServiceInstance, 'getPassagesBetween2Buildings').returns(Result.ok<IPassageDTO>({ id:'sad', building1: 'A', building2: 'B', pisobuilding1: 'A1', pisobuilding2: 'B2', positionBuilding1:[],positionBuilding2:[]}));
+        let buildingRepoInstance = Container.get(BuildingRepo);
+        Container.set('BuildingRepo', buildingRepoInstance);
+        let buildingServiceClass = require(config.services.building.path).default;
+        let buildingServiceInstance = Container.get(buildingServiceClass);
+        Container.set(config.services.building.name, buildingServiceInstance);
+        buildingServiceInstance = Container.get(config.services.building.name);
+        let floorRepoInstance = Container.get(FloorRepo);
+        Container.set('FloorRepo', floorRepoInstance);
+        let floorServiceClass = require(config.services.floor.path).default;
+        let floorServiceInstance = Container.get(floorServiceClass);
+        Container.set(config.services.floor.name, floorServiceInstance);
+        floorServiceInstance = Container.get(config.services.floor.name);
+        sinon.stub(passageServiceInstance, 'getPassagesBetween2Buildings').returns(Result.ok<IPassageDTO[]>([{
+             id: 'sad', building1: 'A', building2: 'B', pisobuilding1: 'A1', pisobuilding2: 'B2', positionBuilding1: [], positionBuilding2: [] }]));
         const ctrl = new PassageController(passageServiceInstance as IPassageService);
-        await ctrl.updatePassage(<Request>req, <Response>res, <NextFunction>next);
+        await ctrl.getPassagesBetween2Buildings(<Request>req, <Response>res, <NextFunction>next);
         sinon.assert.calledOnce(res.json);
-        sinon.assert.calledWith(res.json, sinon.match({
-            id:'sad',
+        sinon.assert.calledWith(res.json, sinon.match([{
+            id: 'sad',
             building1: 'A',
             building2: 'B',
             pisobuilding1: 'A1',
             pisobuilding2: 'B2',
-            positionBuilding1:[],
-            positionBuilding2:[]
-        }));
+            positionBuilding1: [],
+            positionBuilding2: []
+        }]));
     });
+
+
 }); 
